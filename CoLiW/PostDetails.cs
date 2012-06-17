3532d96fcaac99e95275c44860a2e2ecfa452af4
unknown <bogdy016@yahoo.com>
2012-06-17 17:53:42 +0300

    added postdetails class

---
+++ CoLiW/PostDetails.cs
@@ -0,0 +1,17 @@
+namespace CoLiW
+{
+    public class PostDetails
+    {
+        public string Name { get; set; }
+
+        public string Link { get; set; }
+
+        public string Caption { get; set; }
+
+        public string Description { get; set; }
+
+        public string Message { get; set; }
+
+        public string PictureUrl { get; set; }
+    }
+}
