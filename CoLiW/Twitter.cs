477adeed7395630e96f9136eef80b41493e590f5
unknown <bogdy016@yahoo.com>
2012-06-19 01:03:38 +0300

    twitter login

---
+++ CoLiW/Twitter.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using TweetSharp;
+
+namespace CoLiW
+{
+    public class Twitter : TwitterService
+    {
+        public Twitter()
+        {
+
+        }
+
+        public Twitter(string consumerKey, string consumerSecret)
+            : base(consumerKey, consumerSecret)
+        {
+            ConsumerKey = consumerKey;
+            ConsumerSecret = consumerSecret;
+            LoginForm = new TwitterLoginForm();
+            LoginForm.Browser.Navigated += BrowserNavigated;
+
+        }
+
+        void BrowserNavigated(object sender, System.Windows.Forms.WebBrowserNavigatedEventArgs e)
+        {
+            var htmlString = LoginForm.Browser.DocumentText;
+            Regex expression = new Regex(@"<code>(?<word>\w+)</code>");
+            Match match = expression.Match(htmlString);
+            Pin = match.Groups["word"].Value;
+            if (!string.IsNullOrEmpty(Pin) && Pin.Length == 7)
+            {
+                LoginForm.DialogResult = DialogResult.OK;
+                LoginForm.Close();
+
+            }
+        }
+
+        public TwitterLoginForm LoginForm { get; set; }
+
+        public string ConsumerKey { get; set; }
+
+        public string ConsumerSecret { get; set; }
+
+        public string AccessToken { get; set; }
+
+        public string Pin { get; set; }
+
+        public bool Login(bool forcedLogin)
+        {
+            try
+            {
+                if (LoginForm.IsLoggedIn && forcedLogin == false)
+                    //if the user is logged in and force login = false, then exit
+                    return true;
+                if (forcedLogin && LoginForm.IsLoggedIn)
+                    //if the user is logged in, and forcelogin = true, then logout first
+                    Logout();
+
+                if (Token != null && TokenSecret != null)
+                {
+                    AuthenticateWith(Token, TokenSecret);
+                    return true;
+                }
+                // Step 1 - Retrieve an OAuth Request Token
+                OAuthRequestToken requestToken = GetRequestToken("oob");
+                
+                // Step 2 - Redirect to the OAuth Authorization URL
+                Uri uri = GetAuthorizationUri(requestToken);
+                LoginForm.Browser.Navigate(uri);
+                //Console.WriteLine("A 7-digit code will appear in the window after you authorize the app.\nPlease copy that code, close the window, and insert it in this console and hit <Enter> key:");
+                DialogResult loginResult = LoginForm.ShowDialog();
+                if (loginResult != DialogResult.OK)
+                {
+                    Console.WriteLine("Login was unsuccesful");
+                    return false;
+                }
+                OAuthAccessToken access = GetAccessToken(requestToken, Pin);
+                AuthenticateWith(access.Token, access.TokenSecret); Token = access.Token;
+                TokenSecret = access.TokenSecret;
+                Token = access.Token;
+                return true;
+            }
+            catch (Exception exception)
+            {
+                throw new InvalidCredentialException(exception.ToString());
+            }
+        }
+
+        private void Logout()
+        {
+
+        }
+
+        public string TokenSecret { get; set; }
+
+        public string Token { get; set; }
+
+        public bool Login(string consumerId, string consumerSecret, bool forcedLogin)
+        {
+            return false;
+        }
+
+        public TwitterStatus GetRetweet(int index)
+        {
+            IEnumerable<TwitterStatus> listRetweetsByMe = ListRetweetsByMe();
+            if(listRetweetsByMe == null)
+                throw new NullReferenceException("There are no retweets");
+            int count = listRetweetsByMe.Count();
+            if(index < 0 || index > count)
+                throw new IndexOutOfRangeException("There are only " + listRetweetsByMe.Count());
+            return listRetweetsByMe.ElementAt(index);
+        }
+    
+        public bool Follow(string username)
+        {
+            string url = "https://api.twitter.com/1/friendships/create.json";
+            return true;
+        }
+    }
+}
---
+++ CoLiW/TwitterLoginForm.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace CoLiW
+{
+    partial class TwitterLoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browser = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // browser
+            // 
+            this.browser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.browser.Location = new System.Drawing.Point(0, 0);
+            this.browser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.browser.Name = "browser";
+            this.browser.Size = new System.Drawing.Size(284, 262);
+            this.browser.TabIndex = 0;
+            // 
+            // TwitterLoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.browser);
+            this.Name = "TwitterLoginForm";
+            this.Text = "TwitterLoginForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser browser;
+    }
+}
