5180d5bc50364d1b70e260eac804ea485f15a3c5
unknown <bogdy016@yahoo.com>
2012-06-23 12:19:56 +0300

    Added GPL License

--- CoLiW/IWebApp.cs
+++ CoLiW/IWebApp.cs
@@ -1,3 +1,30 @@
+#region License
+
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="{Faculty of Computer Science A. I. Cuza}">
+//
+// CoLiW is an eperiment for using web applications in the command line
+// Copyright (C) 2012 Faculty of Computer Science A. I. Cuza
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the +terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/. 
+// </copyright>
+// <summary>
+// Email: coliw@gmail.com
+// </summary>
+// -------------------------------------------------------------------------------------------------------------------
+
+#endregion
 namespace CoLiW
 {
     internal interface IWebApp
